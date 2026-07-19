f4dc2cbb3a3c4c7bc98fc515527b10b03c9be37f
Herobroto <herobrotelpro@gmail.com>
2026-07-19 02:39:37 -0600

    validacion de tipos mejor registro

--- src/main/java/com/herobrot/heroslib/attachment/AttachmentRegistryHelper.java
+++ src/main/java/com/herobrot/heroslib/attachment/AttachmentRegistryHelper.java
@@ -11,26 +11,59 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
-@SuppressWarnings("unused")
 public class AttachmentRegistryHelper {
 
+    @SuppressWarnings("unused")
     public static DeferredRegister<AttachmentType<?>> createRegister(String modId) {
         return DeferredRegister.create(NeoForgeRegistries.Keys.ATTACHMENT_TYPES, modId);
     }
 
-    public static <T extends INBTSyncable> Supplier<AttachmentType<T>> registerNBTAttachment(
+    /**
+     * Registra un Attachment basado en NBT con validación estricta de tipos.
+     */
+    @SuppressWarnings("unused")
+    public static <H extends IAttachmentHolder, T extends INBTSyncable> Supplier<AttachmentType<T>> registerNBTAttachment(
             DeferredRegister<AttachmentType<?>> register,
             String name,
-            Function<IAttachmentHolder, T> factory,
+            Class<H> holderType,
+            Function<H, T> factory,
             boolean copyOnDeath
     ) {
+        // Llama a la función principal con un modificador vacío (identity)
+        return registerNBTAttachment(register, name, holderType, factory, copyOnDeath, UnaryOperator.identity());
+    }
+
+    /**
+     * Registra un Attachment basado en NBT permitiendo modificar el Builder interno de NeoForge (Ej. para añadir .sync()).
+     */
+    @SuppressWarnings("unused")
+    public static <H extends IAttachmentHolder, T extends INBTSyncable> Supplier<AttachmentType<T>> registerNBTAttachment(
+            DeferredRegister<AttachmentType<?>> register,
+            String name,
+            Class<H> holderType,
+            Function<H, T> factory,
+            boolean copyOnDeath,
+            UnaryOperator<AttachmentType.Builder<T>> customizer
+    ) {
+        // Fábrica segura: Valida el tipo antes de hacer el cast
+        Function<IAttachmentHolder, T> safeFactory = holder -> {
+            if (!holderType.isInstance(holder)) {
+                throw new IllegalStateException(
+                        "HerosLib: El Attachment '" + name + "' esperaba un holder de tipo " + holderType.getSimpleName()
+                                + " pero recibio " + holder.getClass().getSimpleName()
+                );
+            }
+            return factory.apply(holderType.cast(holder));
+        };
+
         return register.register(name, () -> {
-            var builder = AttachmentType.builder(factory)
+            var builder = AttachmentType.builder(safeFactory)
                     .serialize(new IAttachmentSerializer<CompoundTag, T>() {
                         @Override
                         public @NotNull T read(@NotNull IAttachmentHolder holder, @NotNull CompoundTag tag, HolderLookup.@NotNull Provider provider) {
-                            T instance = factory.apply(holder);
+                            T instance = safeFactory.apply(holder);
                             instance.readNbt(tag);
                             return instance;
                         }
@@ -43,9 +76,12 @@ public class AttachmentRegistryHelper {
                         }
                     });
 
-            if (copyOnDeath) builder.copyOnDeath();
+            if (copyOnDeath) {
+                builder.copyOnDeath();
+            }
 
-            return builder.build();
+            // Aplicamos cualquier personalización extra que el mod dependiente haya solicitado
+            return customizer.apply(builder).build();
         });
     }
 
